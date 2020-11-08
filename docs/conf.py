74adae35913548affa99cbb86d3100314606ac2d
naman <naman25295@gmail.com>
2020-11-07 20:24:48 -0500

    Final linting changes

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','recommonmark']
+extensions = ['sphinx.ext.autodoc', 'recommonmark']
 source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-get_html_theme_path=[sphinx_rtd_theme.get_html_theme_path()]
+get_html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
